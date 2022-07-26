261164489e7c12b44f323cb2ece23c29caaac472
rajasekhar8055 <110010879+rajasekhar8055@users.noreply.github.com>
2022-07-26 12:42:28 +0530

    raja.py

--- Main.py
+++ Main.py
@@ -6,20 +6,45 @@ class Stack:
 
     def is_empty(self):
         # Write code here
+        if self.top==-1
+              return 1
+        else:
+            return 0
+        
+        
 
     def is_full(self):
         # Write code here
+        if self.top==(self.size-1):
+            return 1
+        else:
+            return 0
 
     def push(self, data):
         if not self.is_full():
             # Write code here
+            self.top+=1 self.ist[self.top]=data
+            print("ELEMENT IS SUCCESFULLY ADDED")
+            else:
+                print(("Stack is full")
+                      print("cannont push an element")
 
     def pop(self):
         if not self.is_empty():
             # Write code here
+                      t=self.ist[self.top]
+                      delself.ist[self.top]
+                      self.top-=1
+                      return t
+                      else:
+                      return"stackis empty"
+                      return"cannont pop an element"
 
     def status(self):
         # Write code here
+                      for element in self.items:
+                      print(element)
+                      
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
